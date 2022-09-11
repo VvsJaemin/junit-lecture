db5cca44038d2fb492d81f6f950cb9e57ab6d08f
VvsJaemin <djd453216@nate.com>
2022-09-11 21:11:23 +0900

    feat : junit select 테스트

--- src/test/java/site/meta/junit/domain/BookRepositoryTest.java
+++ src/test/java/site/meta/junit/domain/BookRepositoryTest.java
@@ -1,10 +1,12 @@
 package site.meta.junit.domain;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.PrePersist;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,8 +16,21 @@ public class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+//    @BeforeAll // 테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    void testData() {
+        String title = "junit5";
+        String author = "jm2";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        bookRepository.save(book);
+    }
+
     @Test
-    public void 책등록_test() {
+    void 책등록_test() {
         //given(데이터 준비)
         String title = "junit5";
         String author = "jm";
@@ -29,5 +44,29 @@ public class BookRepositoryTest {
         //then(검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
+        //트랜잭션 종료 -> 데이터 초기화
+    }
+
+    @Test
+    void 책목록보기_test() {
+        //given
+        testData();
+        //when
+        List<Book> bookPS = bookRepository.findAll();
+        //then
+        System.out.println("books.size() = " + bookPS.size());
+        assertEquals("junit5", bookPS.get(0).getTitle());
+        assertEquals("jm2", bookPS.get(0).getAuthor());
+    }
+
+    @Test
+    void 책한건보기_test() {
+        //given
+        testData();
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
+        //then
+        assertEquals("junit5", bookPS.getTitle());
+        assertEquals("jm2", bookPS.getAuthor());
     }
 }
